fbc3568c9c813c63044fb7e9c5abc77f36b09273
Natasha Ruf <natasha.ruf@gmail.com>
2018-01-30 20:41:47 +0100

    <accordion> auszuprobieren

--- js/vendor/jquery.js
+++ js/vendor/jquery.js
@@ -10251,3 +10251,11 @@ if ( !noGlobal ) {
 
 return jQuery;
 } );
+
+
+$(document).ready(function(){
+	$("dt").click(function(){ // trigger
+		$(this).next("dd").slideToggle("fast"); // blendet beim Klick auf "dt" die nächste "dd" ein.
+		$(this).children("a").toggleClass("closed open"); // wechselt beim Klick auf "dt" die Klasse des enthaltenen a-Tags von "closed" zu "open".
+	});
+});
